0d35d335c09737e2e0d50af6df6a1a28ed40c28d
DnYorch <jorgelopezarevalo0@gmail.com>
2024-03-26 20:35:04 -0600

    mi primer commit

---
+++ src/public/js/main.js
@@ -0,0 +1,40 @@
+var map = L.map('map-template').setView([21.9878131898026, -98.99167051286172], 14);
+
+const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
+const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+
+const tile = L.tileLayer(tileURL2);
+
+// Socket Io
+const socket = io.connect();
+
+// Marker
+const marker = L.marker([21.98186388714534, -99.01083221399061]); // Cd. Valles, S.L.P.
+marker.bindPopup('Hello There!');
+map.addLayer(marker);
+
+// Geolocation
+map.locate({enableHighAccuracy: true})
+map.on('locationfound', (e) => {
+  const coords = [e.latlng.lat, e.latlng.lng];
+  const newMarker = L.marker(coords);
+  newMarker.bindPopup('You are Here!');
+  map.addLayer(newMarker);
+  socket.emit('userCoordinates', e.latlng);
+});
+
+// socket new User connected
+socket.on('newUserCoordinates', (coords) => {
+  console.log(coords);
+  const userIcon = L.icon({
+    iconUrl: '/img/icon2.png',
+    iconSize: [38, 42],
+  })
+  const newUserMarker = L.marker([coords.lat, coords.lng], {
+    icon: userIcon 
+  });
+  newUserMarker.bindPopup('New User!');
+  map.addLayer(newUserMarker);
+}); 
+
+map.addLayer(tile);
